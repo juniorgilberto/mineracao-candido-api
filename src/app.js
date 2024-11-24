849212d4c02bd64e4fa6d3141516333bce75dc4e
Gilberto Junior <56450453+juniorgilberto@users.noreply.github.com>
2024-11-24 14:54:10 -0400

    Add Cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 // Esse arquivo server pra centralizar as configs do projeto;
 //cors, middlewars.. etc;
 import express from "express";
+import cors from 'cors'
 import index from "./Routes/index.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 //Rotas
 app.use('/', index);
